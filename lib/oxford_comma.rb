79b684503ee124b757ca2629947235210565802a
Theo Ferguson <ferguson.theo@gmail.com>
2021-09-23 09:58:58 -0400

    commit tree 82b7a925c9001003d926f76988168588f37578dc

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length > 1
+    array[-1] = "and " << array[-1]
+    end
+    if array.length > 2
+     array.join(", ")
+    else 
+        array.join(" ")
+    end
 end
+
+
